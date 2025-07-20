2e9ffc9bc611c5fcc8fcf9f4992ba0c7259b6cbd
rosinunes84 <rosiane.nunes84@gmail.com>
2025-07-20 01:37:32 -0300

    Delete lavacar directory

--- lavacar/js/logout.js
+++
@@ -1,6 +0,0 @@
-import { supabase } from './supabase-config.js';
-
-document.getElementById('btn-logout').addEventListener('click', async () => {
-  await supabase.auth.signOut();
-  window.location.href = 'login.html';
-});
